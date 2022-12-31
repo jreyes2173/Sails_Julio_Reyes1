562022aa228834cbc4e594d5dc640a5df190fc0e
julio <avalancha36@hotmail.com>
2022-12-30 22:40:30 -0400

    proyecto de prueba2

---
+++ api/controllers/PrincipalController.js
@@ -0,0 +1,45 @@
+/**
+ * PrincipalController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+  inicio: async (peticion, respuesta) => {
+    let fotos = await Foto.find({ activa: true, tipo: 1 }).sort("id")
+    respuesta.view('pages/inicio', { fotos })
+  },
+
+  ropas: async (peticion, respuesta) => {
+    let fotos = await Foto.find({ activa: true, tipo: 2 }).sort("id")
+    respuesta.view('pages/ropas', { fotos })
+  },
+
+  topVendidas: async (peticion, respuesta) => {
+
+    let consulta = `
+    SELECT
+      titulo,
+      contenido,
+      COUNT ( * ) AS cantidad
+    FROM
+      orden_detalle
+      INNER JOIN foto ON orden_detalle.foto_id = foto.ID
+    GROUP BY
+      titulo, contenido, foto_id
+    ORDER BY
+    COUNT ( * ) DESC
+    LIMIT 10
+    `
+
+    await OrdenDetalle.query(consulta, [], (errores, resultado) => {
+      let fotos = resultado.rows
+      respuesta.view('pages/top_vendidas', { fotos })
+    })
+  }
+
+
+};
+
